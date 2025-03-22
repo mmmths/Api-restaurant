0651243e345d02613c658cc552cadba6cca68762
Matheus Veiga Lima <mveigadelima@gmail.com>
2025-03-22 17:13:38 -0300

    Controller: creating update and remove

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,10 +1,14 @@
 import { AppError } from "@/utils/AppError";
 import { NextFunction, Request, Response } from 'express';
-
+import {ZodError} from 'zod'
 
 export function errorHandler(error:any, request: Request, response: Response, _:NextFunction) {
      if(error instanceof AppError){
           return response.status(error.statutsCode).json({message: error.message})
+     }
+
+     if(error instanceof ZodError){
+          return response.status(400).json({message: "validation error", issues: error.format()})
      }
 
      return response.status(500).json({message: error.message})
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -9,4 +9,7 @@ export const productsRoutes =  Router()
 const producsController = new ProductsController()
 
 productsRoutes.get('/', producsController.index)
+productsRoutes.post('/', producsController.create)
+productsRoutes.put('/:id', producsController.update)
+productsRoutes.delete('/:id', producsController.remove)
 
